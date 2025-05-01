73dbe943e3afd335995c84ce7ae8341f577d71a5
lucas-lob <lucas.mcp23@gmail.com>
2025-05-01 09:54:50 -0300

    :lipstick: feat: stylizes the Pomodoro

--- src/assets/components/Pomodoro/Pomodoro.jsx
+++ src/assets/components/Pomodoro/Pomodoro.jsx
@@ -3,7 +3,21 @@ import { useState } from 'react'
 
 import style from './Pomodoro.module.css'
 
+const IconButton = ({ opacity, iconText, rescale = 1 }) => {
+    return (
+        <button type='button'>
+            <span className={`scale-[${rescale}] md:!text-[64px] !text-primary/${opacity} hover:!text-tertiary/${opacity} transition duration-300 ease-in-out material-symbols-rounded`}>
+                {iconText}
+            </span>
+        </button>
+    )
+}
+
 export const Pomodoro = () => {
+    // CONSTANTS
+    const USABLE_OPACITY = 100
+    const UNUSABLE_OPACITY = 30
+
     const [timerValues, setTimerValues] = useState({
         hours: 0,
         minutes: 0,
@@ -12,40 +26,34 @@ export const Pomodoro = () => {
 
     const [currentSection, setCurrentSection] = useState(1)
 
+    const [elementsOpacity, setElementsOpacity] = useState({
+        timer: UNUSABLE_OPACITY,
+        section: UNUSABLE_OPACITY,
+        playButton: USABLE_OPACITY,
+        pauseButton: UNUSABLE_OPACITY,
+        stopButton: UNUSABLE_OPACITY,
+        settingsButton: USABLE_OPACITY
+    })
+
     return (
-        <div className="absolute left-[50%] top-[50%] flex flex-col items-center">
-            <h1>
+        <div className="absolute left-[50%] top-[50%] translate-[-50%] flex flex-col items-center">
+            <h1 className={`!text-[50px] sm:!text-[75px] lg:!text-[100px] xl:!text-[150px] !text-primary/${elementsOpacity.timer}`}>
                 {timerValues.hours} : {timerValues.minutes} : {timerValues.seconds}
             </h1>
 
-            <h2>
+            <h2 className={`mb-lg lg:mb-xl !text-primary/${elementsOpacity.section}`}>
                 Sessão {currentSection}
             </h2>
 
-            <div className='flex'>
-                <button type='button'>
-                    <span class="material-symbols-rounded">
-                        play_arrow
-                    </span>
-                </button>
+            <div className='flex gap-[20px] lg:gap-[40px]'>
 
-                <button type='button'>
-                    <span class="material-symbols-rounded">
-                        pause
-                    </span>
-                </button>
+                <IconButton opacity={elementsOpacity.playButton} iconText="play_arrow" />
 
-                <button type='button'>
-                    <span class="material-symbols-rounded">
-                        stop
-                    </span>
-                </button>
+                <IconButton opacity={elementsOpacity.pauseButton} iconText="pause" />
 
-                <button type='button'>
-                    <span class="material-symbols-rounded">
-                        settings
-                    </span>
-                </button>
+                <IconButton opacity={elementsOpacity.stopButton} iconText="stop" />
+
+                <IconButton opacity={elementsOpacity.settingsButton} iconText="settings" rescale ={0.7} />
             </div>
         </div>
     )
