815359e8205c0575b904935a250ea2cdc5eee056
lucas-lob <lucas.mcp23@gmail.com>
2025-05-02 14:32:40 -0300

    :sparkles: feat: creates function to the converter a total seconds amount in hours, minutes and seconds

--- src/assets/components/Pomodoro/timeConverter.js
+++ src/assets/components/Pomodoro/timeConverter.js
@@ -15,3 +15,25 @@ export function secondsToFormattedTime(seconds){
 
     return hoursValue + ":" + minutesValue + ":" + secondsValue
 }
+
+/**
+ * Convert a total seconds value to the hours, minutes and seconds
+ * 
+ * @param seconds - total seconds amount
+ * @return a object contained hours, minutes and seconds
+ */
+export function secondsToTime(seconds){
+    let hoursValue
+    let minutesValue
+    let secondsValue
+
+    hoursValue = Math.trunc(seconds / 3600)
+    minutesValue = Math.trunc(seconds / 60 - hoursValue * 60)
+    secondsValue = seconds - minutesValue * 60 - hoursValue * 3600
+
+    return {
+        hours: hoursValue,
+        minutes: minutesValue,
+        seconds: secondsValue
+    }
+}
