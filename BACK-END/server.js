9eb5a41ba775a599fa75ad49ad5c66de00fb299a
lucas-lob <lucas.mcp23@gmail.com>
2025-05-07 12:39:03 -0300

    :sparkles: feat: creates user API routers
    Routers created: post, put, delete

--- BACK-END/server.js
+++ BACK-END/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { PrismaClient } from './generated/prisma/index.js'
 
 /* 
 MONGO DB LOGIN:
@@ -7,9 +8,60 @@ iJn4AspRDOyg8uMG
 */
 
 const app = express()
+app.use(express.json())
 
-app.get('/users', (req, res) => {
-    res.send("Users router")
+const prisma = new PrismaClient()
+
+
+
+app.post('/users', async (req, res) => {
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        }
+    })
+
+    res.status(201).json({ message: "User created" })
 })
+
+
+
+app.put('/users/:id', async (req, res) => {
+    try {
+        await prisma.user.update({
+            where: {
+                id: req.params.id
+            },
+            data: {
+                name: req.body.name,
+                email: req.body.email
+            }
+        })
+    } catch {
+        res.status(200).json({ message: "User ID not found" })
+    }
+
+    res.status(200).json({ message: "User updated" })
+})
+
+
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        await prisma.user.delete({
+            where: {
+                id: req.params.id
+            }
+        })
+    } catch {
+        res.status(200).json({ message: "User ID not found" })
+    }
+
+    res.status(200).json({ message: "User deleted" })
+})
+
+
 
 app.listen(3000)
